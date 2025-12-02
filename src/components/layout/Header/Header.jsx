5fc79d8c7a65db87f454db9ad6fe0c3db19270ff
MatiasNSticca <sticcamatias.dev@gmail.com>
2025-12-02 13:44:05 -0300

    feat: migrar web a react + estructuracion de carpetas | Version: 1.0

---
+++ src/components/layout/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+import HeaderNav from "./HeaderNav";
+import Hero from "./Hero";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <HeaderNav 
+        isOpen={isOpen} 
+        toggleMenu={() => setIsOpen(!isOpen)} 
+      />
+
+      <Hero />
+    </header>
+  );
+}
+
+export default Header;
